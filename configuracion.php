61e531a4ba9ac135e532390e18c489ca0a3622ec
root <root@apps.esi.edu.uy>
2024-05-23 18:16:59 -0300

    Primera version, sin compose

---
+++ configuracion.php
@@ -0,0 +1,6 @@
+<?php 
+	
+	$DB_IP=getenv("DB_IP");
+	$DB_USER=getenv("DB_USER");
+	$DB_PASSWORD=getenv("DB_PASSWORD");
+	$DB_NAME=getenv("DB_NAME");
---
+++ index.php
@@ -0,0 +1,15 @@
+<?php 
+	
+	require "configuracion.php";
+	
+
+	echo "<pre>";
+
+	echo $DB_IP . "\n";
+        echo $DB_USER . "\n";
+        echo $DB_PASSWORD . "\n";
+        echo $DB_NAME . "\n";
+
+
+	
+	
